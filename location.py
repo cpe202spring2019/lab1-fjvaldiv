85526859bece2db0c6bf29add949b38510dd6cd2
fjvaldiv <fjvaldiv@calpoly.edu>
2019-04-10 12:27:38 -0700

    I made these changes

--- location.py
+++ location.py
@@ -7,9 +7,11 @@ class Location:
         self.lat = lat      # latitude in degrees (-90 to 90)
         self.lon = lon      # longitude in degrees (-180 to 180)
 
+    #Defines the equality for Location class
     def __eq__(self, other):
         return (type(other) == Location and self.name == other.name and self.lat == other.lat and self.lon == other.lon)
 
+    #Defines how objects in this class should be represented
     def __repr__(self):
         return ('Location('"'{}'"', {}, {})'.format(self.name, self.lat, self.lon))
 
